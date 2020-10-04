c1d262e66a378debd2ab2c96eeab36fbface361d
fari990 <70996770+fari990@users.noreply.github.com>
2020-10-04 20:09:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var object;
+
+function setup(){
+  var canvas = createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  object= Bodies.rectangle(200,100,50,50);
+  World.add(world.object);
+}
+
+function draw(){
+  background(0,0,0); 
+  rectMode(CENTER); 
+  rect(200,200,50,50)
+  
+}
